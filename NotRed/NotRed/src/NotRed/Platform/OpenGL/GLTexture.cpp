006f739d723eba016e7937aedf4a5beaa0a0ec00
AnselmoNoriega <anselmonoriega3@gmail.com>
2024-09-07 22:52:48 -0700

    temporal change for image filtering

--- NotRed/NotRed/src/NotRed/Platform/OpenGL/GLTexture.cpp
+++ NotRed/NotRed/src/NotRed/Platform/OpenGL/GLTexture.cpp
@@ -95,8 +95,9 @@ namespace NR
                 glGenTextures(1, &instance->mID);
                 glBindTexture(GL_TEXTURE_2D, instance->mID);
 
-                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+                //TODO: make this toggleable between GL_LINEAR && GL_NEAREST
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+                glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
                 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
                 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
                 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
