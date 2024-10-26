b792472af24fdc530db07e0e3bfb3aa03a4434bd
AnselmoNoriega <anselmonoriega3@gmail.com>
2024-10-25 18:06:07 -0700

    Image update

--- NotRed/NotRed/src/NotRed/Platform/Vulkan/VKImage.cpp
+++ NotRed/NotRed/src/NotRed/Platform/Vulkan/VKImage.cpp
@@ -129,7 +129,7 @@ namespace NR
 		}
 		else if (mSpecification.Usage == ImageUsage::Storage)
 		{
-			usage |= VK_IMAGE_USAGE_TRANSFER_SRC_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT | VK_IMAGE_USAGE_SAMPLED_BIT | VK_IMAGE_USAGE_STORAGE_BIT;
+			usage |= VK_IMAGE_USAGE_STORAGE_BIT | VK_IMAGE_USAGE_TRANSFER_DST_BIT;
 		}
 
 		VkImageAspectFlags aspectMask = Utils::IsDepthFormat(mSpecification.Format) ? VK_IMAGE_ASPECT_DEPTH_BIT : VK_IMAGE_ASPECT_COLOR_BIT;
