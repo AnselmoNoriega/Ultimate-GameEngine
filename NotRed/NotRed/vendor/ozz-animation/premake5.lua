32ce0e2a384b23d8ee6da0a987a26047af0a5a8e
AnselmoNoriega <anselmonoriega3@gmail.com>
2025-01-02 21:44:20 -0600

    Setting things up for the running the project with fbx sdk

---
+++ NotRed/NotRed/vendor/ozz-animation/premake5.lua
@@ -0,0 +1,181 @@
+project "ozz_base"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/ozz/base/**.h",
+		"src/base/**.cc"
+	}
+	includedirs
+	{
+		"include",
+		"src"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+
+project "ozz_animation"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+	    "include/ozz/animation/runtime/**.h",
+		"src/animation/runtime/**.h",
+		"src/animation/runtime/**.cc"
+	}
+
+	includedirs
+	{
+		"include",
+		"src"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+
+project "ozz_geometry"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+	
+	files
+	{
+	    "include/ozz/geometry/runtime/**.h",
+		"src/geometry/runtime/**.h",
+		"src/geometry/runtime/**.cc"
+	}
+
+	includedirs
+	{
+		"include",
+		"src"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+
+project "ozz_animation_offline"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	-- wildcards are not recursive here, on purpose
+	files
+	{
+		"include/ozz/animation/offline/*.h",
+		"src/animation/offline/*.h",
+		"src/animation/offline/*.cc"
+	}
+
+	includedirs
+	{
+		"include",
+		"src"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+
+project "ozz_animation_fbx"
+	kind "StaticLib"
+	language "C++"
+	cppdialect "C++17"
+	staticruntime "off"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"include/ozz/animation/offline/fbx/fbx.h",
+		"include/ozz/animation/offline/fbx/fbx_animation.h",
+		"include/ozz/animation/offline/fbx/fbx_skeleton.h",
+		"src/animation/offline/fbx/fbx.cc",
+		"src/animation/offline/fbx/fbx_animation.cc",
+		"src/animation/offline/fbx/fbx_skeleton.cc"
+	}
+
+	includedirs
+	{
+		"include",
+		"src"
+		"%{IncludeDir.FBX}"
+	}
+
+	filter "system:windows"
+		systemversion "latest"
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+		
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
